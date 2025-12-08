02bc7d82f8e969d47fe86356be7dfc72de9b1e90
prajithravisankar <prajithravisankar@gmail.com>
2025-12-07 23:04:29 -0500

    test

---
+++ my-app/app/touring/page.tsx
@@ -0,0 +1,37 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { InputWithButton } from "@/components/input-with-button"
+import { Itinerary } from "@/components/itinerary"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
+    return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "350px",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebar />
+            <div className="flex flex-1 flex-col gap-0 p-3">
+                <SidebarInset className="rounded-xl border border-border bg-card/50">
+                    <header className="flex h-16 shrink-0 items-center gap-2">
+                        <div className="flex flex-1 items-center gap-2 px-4">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator
+                                orientation="vertical"
+                                className="mr-2 data-[orientation=vertical]:h-4"
+                            />
+                            <InputWithButton />
+                        </div>
+                    </header>
+                </SidebarInset>
+            </div>
+        </SidebarProvider>
+    )
+}
