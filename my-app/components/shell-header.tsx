bcd413d20c6b30a8f623e8d252c959e9701831f2
Srijan Ravisankar <srijanvr@gmail.com>
2025-12-08 23:07:57 -0500

    Fix the layout issue in the home page

--- my-app/components/shell-header.tsx
+++ my-app/components/shell-header.tsx
@@ -15,13 +15,6 @@ export function ShellHeader() {
     return (
         <header className="flex h-16 shrink-0 items-center gap-2">
             <div className="flex flex-1 items-center gap-2 px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator
-                    orientation="vertical"
-                    className="mr-2 data-[orientation=vertical]:h-4"
-                />
-
-                {/* Only this part changes per route */}
                 {isTouring ? <TouringHeaderActions /> : <InputWithButton />}
             </div>
         </header>
