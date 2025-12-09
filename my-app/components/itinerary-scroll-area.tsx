32e9c3ccce974bd7643fdcc304fb533ad81705bd
Srijan Ravisankar <srijanvr@gmail.com>
2025-12-09 11:17:01 -0500

    Add a basic journey timeline with mock data

--- my-app/components/itinerary-scroll-area.tsx
+++ my-app/components/itinerary-scroll-area.tsx
@@ -1,11 +1,14 @@
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { SidebarUserAccordion } from "./user-accordion"
+import { HomeSidebarContent } from "./sidebar/home-sidebar-content"
+import { JourneyTimeline } from "./journey-timeline"
 
 export function ItineraryScrollArea() {
   return (
     <ScrollArea className="h-125 overflow-y-auto w-full rounded-md border bg-muted/50 pb-auto">
       <div className="p-4">
-        <SidebarUserAccordion />
+        {/* <SidebarUserAccordion /> */}
+        <JourneyTimeline />
       </div>
     </ScrollArea>
   )
--- my-app/components/shell-layout.tsx
+++ my-app/components/shell-layout.tsx
@@ -4,37 +4,37 @@ import React from "react"
 
 import { AppSidebar } from "@/components/app-sidebar"
 import {
-    SidebarProvider,
-    SidebarInset,
+  SidebarProvider,
+  SidebarInset,
 } from "@/components/ui/sidebar"
 import { ShellHeader } from "@/components/shell-header"
 
 export default function ShellLayout({
-                                        children,
-                                    }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode
 }) {
-    return (
-        <SidebarProvider
-            style={
-                {
-                    "--sidebar-width": "350px",
-                } as React.CSSProperties
-            }
-        >
-            <AppSidebar />
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
 
-            <div className="flex flex-1 flex-col gap-0 p-3">
-                <SidebarInset className="rounded-xl border border-border bg-card/50">
-                    {/* Header */}
-                    <ShellHeader />
+      <div className="flex flex-1 flex-col gap-0 p-3">
+        <SidebarInset className="rounded-xl border border-border bg-card/50">
+          {/* Header */}
+          <ShellHeader />
 
-                    {/* Content */}
-                    <div className="flex flex-1 flex-col gap-6 p-4 pt-0">
-                        {children}
-                    </div>
-                </SidebarInset>
-            </div>
-        </SidebarProvider>
-    )
+          {/* Content */}
+          <div className="flex flex-1 flex-col gap-6 p-4 pt-0">
+            {children}
+          </div>
+        </SidebarInset>
+      </div>
+    </SidebarProvider>
+  )
 }
