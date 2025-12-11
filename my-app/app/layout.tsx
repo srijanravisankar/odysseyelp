2d50074823eb161fc332ebd8d3c3402aa0c88d2f
Srijan Ravisankar <srijanvr@gmail.com>
2025-12-11 11:54:37 -0500

    Add user context and make the user profile reflect current user

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import ShellLayout from "@/components/shell-layout"
 import { ThemeProvider } from "@/components/theme-provider"
+import { UserProvider } from "@/components/user-context"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <UserProvider>
+            {children}
+          </UserProvider>
         </ThemeProvider>
       </body>
     </html>
