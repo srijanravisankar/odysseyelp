a5f6db04a7825fa296da64d8b075b083f10ce0b0
Srijan Ravisankar <srijanvr@gmail.com>
2025-12-15 22:03:15 -0500

    Remove custom error for enum in Zod schema

--- my-app/components/calendar/schemas.ts
+++ my-app/components/calendar/schemas.ts
@@ -4,13 +4,13 @@ export const eventSchema = z.object({
 	title: z.string().min(1, "Title is required"),
 	description: z.string().min(1, "Description is required"),
 	startDate: z.date({
-		required_error: "Start date is required",
+		message: "Start date is required",
 	}),
 	endDate: z.date({
-		required_error: "End date is required",
+		message: "End date is required",
 	}),
 	color: z.enum(["blue", "green", "red", "yellow", "purple", "orange"], {
-		required_error: "Variant is required",
+		message: "Variant is required",
 	}),
 });
 
