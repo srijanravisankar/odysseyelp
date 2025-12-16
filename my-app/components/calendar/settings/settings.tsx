d2b3817f35f4846e9f260b066de6c3902ad3f9ed
Srijan Ravisankar <srijanvr@gmail.com>
2025-12-15 22:13:11 -0500

    Fix Switch component in settings

--- my-app/components/calendar/settings/settings.tsx
+++ my-app/components/calendar/settings/settings.tsx
@@ -56,15 +56,13 @@ export function Settings() {
 				<DropdownMenuGroup>
 					<DropdownMenuItem>
 						Use dark mode
-						<DropdownMenuShortcut>
+						<DropdownMenuShortcut className="flex items-center gap-2">
+							{isDarkMode ? (
+								<MoonIcon className="h-4 w-4" />
+							) : (
+								<SunMediumIcon className="h-4 w-4" />
+							)}
 							<Switch
-								icon={
-									isDarkMode ? (
-										<MoonIcon className="h-4 w-4" />
-									) : (
-										<SunMediumIcon className="h-4 w-4" />
-									)
-								}
 								checked={isDarkMode}
 								onCheckedChange={(checked) =>
 									setTheme(checked ? "dark" : "light")
@@ -74,15 +72,13 @@ export function Settings() {
 					</DropdownMenuItem>
 					<DropdownMenuItem>
 						Show confirmation dialog on event drop
-						<DropdownMenuShortcut>
+						<DropdownMenuShortcut className="flex items-center gap-2">
+							{showConfirmation ? (
+								<CheckIcon className="h-4 w-4" />
+							) : (
+								<XIcon className="h-4 w-4" />
+							)}
 							<Switch
-								icon={
-									showConfirmation ? (
-										<CheckIcon className="h-4 w-4" />
-									) : (
-										<XIcon className="h-4 w-4" />
-									)
-								}
 								checked={showConfirmation}
 								onCheckedChange={(checked) => setShowConfirmation(checked)}
 							/>
@@ -90,15 +86,13 @@ export function Settings() {
 					</DropdownMenuItem>
 					<DropdownMenuItem>
 						Use dot badge
-						<DropdownMenuShortcut>
+						<DropdownMenuShortcut className="flex items-center gap-2">
+							{isDotVariant ? (
+								<DotIcon className="w-4 h-4" />
+							) : (
+								<PaletteIcon className="w-4 h-4" />
+							)}
 							<Switch
-								icon={
-									isDotVariant ? (
-										<DotIcon className="w-4 h-4" />
-									) : (
-										<PaletteIcon className="w-4 h-4" />
-									)
-								}
 								checked={isDotVariant}
 								onCheckedChange={(checked) =>
 									setBadgeVariant(checked ? "dot" : "colored")
@@ -108,50 +102,48 @@ export function Settings() {
 					</DropdownMenuItem>
 					<DropdownMenuItem>
 						Use 24 hour format
-						<DropdownMenuShortcut>
+						<DropdownMenuShortcut className="flex items-center gap-2">
+							{use24HourFormat ? (
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width={24}
+									height={24}
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth={2}
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									className="icon icon-tabler icons-tabler-outline icon-tabler-clock-24 h-4 w-4"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<path d="M3 12a9 9 0 0 0 5.998 8.485m12.002 -8.485a9 9 0 1 0 -18 0" />
+									<path d="M12 7v5" />
+									<path d="M12 15h2a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-1a1 1 0 0 0 -1 1v1a1 1 0 0 0 1 1h2" />
+									<path d="M18 15v2a1 1 0 0 0 1 1h1" />
+									<path d="M21 15v6" />
+								</svg>
+							) : (
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width={24}
+									height={24}
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth={2}
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									className="icon icon-tabler icons-tabler-outline icon-tabler-clock-12 h-4 w-4"
+								>
+									<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+									<path d="M3 12a9 9 0 0 0 9 9m9 -9a9 9 0 1 0 -18 0" />
+									<path d="M12 7v5l.5 .5" />
+									<path d="M18 15h2a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-1a1 1 0 0 0 -1 1v1a1 1 0 0 0 1 1h2" />
+									<path d="M15 21v-6" />
+								</svg>
+							)}
 							<Switch
-								icon={
-									use24HourFormat ? (
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width={24}
-											height={24}
-											viewBox="0 0 24 24"
-											fill="none"
-											stroke="currentColor"
-											strokeWidth={2}
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											className="icon icon-tabler icons-tabler-outline icon-tabler-clock-24"
-										>
-											<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-											<path d="M3 12a9 9 0 0 0 5.998 8.485m12.002 -8.485a9 9 0 1 0 -18 0" />
-											<path d="M12 7v5" />
-											<path d="M12 15h2a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-1a1 1 0 0 0 -1 1v1a1 1 0 0 0 1 1h2" />
-											<path d="M18 15v2a1 1 0 0 0 1 1h1" />
-											<path d="M21 15v6" />
-										</svg>
-									) : (
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width={24}
-											height={24}
-											viewBox="0 0 24 24"
-											fill="none"
-											stroke="currentColor"
-											strokeWidth={2}
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											className="icon icon-tabler icons-tabler-outline icon-tabler-clock-12"
-										>
-											<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-											<path d="M3 12a9 9 0 0 0 9 9m9 -9a9 9 0 1 0 -18 0" />
-											<path d="M12 7v5l.5 .5" />
-											<path d="M18 15h2a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-1a1 1 0 0 0 -1 1v1a1 1 0 0 0 1 1h2" />
-											<path d="M15 21v-6" />
-										</svg>
-									)
-								}
 								checked={use24HourFormat}
 								onCheckedChange={toggleTimeFormat}
 							/>
